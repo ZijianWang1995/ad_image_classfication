e1ed34dcb0b80492de3f2fa522c2340355a1211e
ZijianWang1995 <zijianwang1995@gmail.com>
2020-03-25 10:25:24 +0000

    other changes

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import pandas_profiling
-import graphviz
+# import pandas_profiling
+# import graphviz
 import matplotlib
 import matplotlib.pyplot as plt 
 from sklearn import tree
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split, cross_val_score, GridSearc
 from sklearn.datasets import load_iris
 from sklearn.metrics import precision_recall_fscore_support,classification_report, confusion_matrix, recall_score, precision_score, accuracy_score, classification_report, f1_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.svm import SVC
 
 matplotlib.use('tkagg') # only MacPro needs this line to plot images
 
@@ -39,3 +41,39 @@ df_data = pd.DataFrame.drop(data, columns = [1558]) #delete the last label colum
 
 #30% data are splitted into test data set, they are shuffled
 X_train, X_test, y_train, y_test = train_test_split(df_data, df_label, test_size = 0.3, random_state = 40)
+
+
+
+
+# Cs = [0.001, 0.01, 0.1, 1,1.5,2,2.5,3,4,5, 10] ## penalty parameter C for the error term. 
+Cs = [0.1]
+gammas = [0.01]
+# gammas = [0.0001,0.001, 0.01, 0.1, 1]
+param_grid = {'C': Cs, 'gamma' : gammas}
+cv = StratifiedShuffleSplit(n_splits=10, random_state=15)
+grid_search = GridSearchCV(SVC(kernel = 'rbf', probability=True), param_grid, cv=cv) ## 'rbf' stands for gaussian kernel
+grid_search.fit(X_train,y_train)
+
+svm_raw = grid_search.best_estimator_
+svm_raw.score(X_train, y_train)
+
+
+y_pred = svm_raw.predict(X_test)
+
+from sklearn.metrics import roc_curve, auc
+#plt.style.use('seaborn-pastel')
+y_score =  svm_raw.decision_function(X_test)
+
+FPR, TPR, _ = roc_curve(y_test, y_score)
+ROC_AUC = auc(FPR, TPR)
+print (ROC_AUC)
+
+plt.figure(figsize =[11,9])
+plt.plot(FPR, TPR, label= 'ROC curve(area = %0.2f)'%ROC_AUC, linewidth= 4)
+plt.plot([0,1],[0,1], 'k--', linewidth = 4)
+plt.xlim([0.0,1.0])
+plt.ylim([0.0,1.05])
+plt.xlabel('False Positive Rate', fontsize = 18)
+plt.ylabel('True Positive Rate', fontsize = 18)
+plt.title('ROC for Titanic survivors', fontsize= 18)
+plt.show()
